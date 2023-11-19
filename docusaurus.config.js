211314b86ce1ea4791a39e22a317d47a2d01def4
Destino <ramphotograph@163.com>
2023-11-19 16:27:13 +0800

    更新 docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,9 +57,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'FAQ wiki',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Logo',
           src: 'img/logo.svg',
         },
         items: [
