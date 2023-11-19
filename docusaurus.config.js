a26918e2a66822fd497baf75670a522e6755ddfe
Destino <ramphotograph@163.com>
2023-11-19 23:11:44 +0800

    更新 docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'FAQ/README',
             position: 'left',
             label: 'Docs',
           },
