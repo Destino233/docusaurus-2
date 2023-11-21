0b804e731eb970db5255ee5b0c2e44bcd45e1221
Destino <111420909+Destino233@users.noreply.github.com>
2023-11-21 18:12:37 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -14,7 +14,7 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   FAQ: [
-    'FAQ/README',
+    'FAQ/FAQ',
     {
       type: 'category',
       label: 'Getting Started',
